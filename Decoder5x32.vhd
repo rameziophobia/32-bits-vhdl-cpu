7e903cd450960d365c1971c9d889191d3da19d95
rameziophobia <40737327+rameziophobia@users.noreply.github.com>
2019-04-30 22:07:29 +0200

    refactor decoder code

--- Decoder5x32.vhd
+++ Decoder5x32.vhd
@@ -1,46 +1,23 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+USE ieee.numeric_std.ALL; 
 entity Decoder5x32 is
-    Port ( i : in  STD_LOGIC_VECTOR (4 downto 0);
+    Port ( --clk : in  STD_LOGIC;
+           i : in  STD_LOGIC_VECTOR (4 downto 0);
            o : out  STD_LOGIC_VECTOR (31 downto 0));
 end Decoder5x32;
 
 architecture Behavioral of Decoder5x32 is
-
-	component Decoder3x8_en is
-		 Port ( i : in  STD_LOGIC_VECTOR (2 downto 0);
-				  en : in  STD_LOGIC;
-				  o : out  STD_LOGIC_VECTOR (7 downto 0));
-	end component;
-
-	component Decoder2x4 is
-		 Port ( i : in  STD_LOGIC_VECTOR (1 downto 0);
-				  o : out  STD_LOGIC_VECTOR (3 downto 0));
-	end component;
-
-	signal decoder0_out: STD_LOGIC_VECTOR (3 downto 0);
 begin
-   
-	decoder0: Decoder2x4 port map (i  => i(4 downto 3),
-                                  o => decoder0_out);
-											 
-	decoder1: Decoder3x8_en port map (i  => i(2 downto 0),
-	                                  en => decoder0_out(0),
-                                     o  => o(7 downto 0));
-												 
-	decoder2: Decoder3x8_en port map (i  => i(2 downto 0),
-	                                  en => decoder0_out(1),
-                                     o  => o(15 downto 8));
-												 
-	decoder3: Decoder3x8_en port map (i  => i(2 downto 0),
-	                                  en => decoder0_out(2),
-                                     o  => o(23 downto 16));
-												 
-	decoder4: Decoder3x8_en port map (i  => i(2 downto 0),
-	                                  en => decoder0_out(3),
-                                     o  => o(31 downto 24));
+
+	process(i) --add clock, remove i
+	variable temp_out: STD_LOGIC_VECTOR (31 downto 0);
+	begin
+		temp_out := (others => '0');
+		temp_out(to_integer(unsigned(i))) := '1';
+		o <= temp_out;
+	end process;
 	
 end Behavioral;
 
