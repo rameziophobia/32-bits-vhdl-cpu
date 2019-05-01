8afe6fcca4fbdc0fab306d2d233347ae93c6bbae
Mohamed <mohamed.xyz@hotmail.com>
2019-05-01 16:50:49 +0200

    fix: (warnings) logic operations shouldn't be in component port

--- OneBitALU.vhd
+++ OneBitALU.vhd
@@ -14,14 +14,21 @@ end OneBitALU;
 
 architecture Behavioral of OneBitALU is
 
+	signal notB: STD_LOGIC;
 	signal tempB: STD_LOGIC;
+	
+	signal AandB: STD_LOGIC;
+	signal AorB: STD_LOGIC;
+	
 	signal sum: STD_LOGIC;
 	
 begin
 
+	notB <= (not b);
+
 	i_Mux2x1: entity work.OneBitMux2x1 port map(
 		i0 		=> b,
-		i1 		=> (not b),
+		i1 		=> notB,
 		sel 		=> sel(2),
 		output 	=> tempB
 	);
@@ -34,12 +41,15 @@ begin
 		sum 	=> sum
 	);
 
+	AandB <= a and b;
+	AorB 	<= a or b;
+
 	i_Mux4x1: entity work.OneBitMux4x1 port map(
-		i0 		=> (a and b),
-		i1 		=> (a or b),
+		i0 		=> AandB,
+		i1 		=> AorB,
 		i2 		=> sum,
 		i3 		=> sum,
-		sel 		=> (sel(1) & sel(0)),
+		sel 		=> sel(1 downto 0),
 		output 	=> output
 	);
 
